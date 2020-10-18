c55ef67f5fecfff2e2b8ffeec072bd4e94144c73
josh <jbweb89@yahoo.com>
2020-10-18 13:20:10 -0400

    word fit

--- src/components/ImageLinkForm/ImageLInkForm.js
+++ src/components/ImageLinkForm/ImageLInkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3 ">
-        {"This Magic Brain wil detect faces in your pictures. Give it a try!"}
+        {"This Magic Brain will detect faces in your pictures. Give it a try!"}
       </p>
       <div className="pa2 center">
         <div className="center pa4 br3 shadow-5 form">
